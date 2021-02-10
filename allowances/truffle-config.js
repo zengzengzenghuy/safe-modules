48a22bfbaa65eceb88529da3a50d091e2064dd34
Victor Porton <porton@narod.ru>
2021-02-10 23:58:04 +0200

    BSC support

--- allowances/truffle-config.js
+++ allowances/truffle-config.js
@@ -84,7 +84,23 @@ module.exports = {
       confirmations: 0,    // # of confs to wait between deployments. (default: 0)
       timeoutBlocks: 200,  // # of blocks before a deployment times out  (minimum/default: 50)
       skipDryRun: true     // Skip dry run before migrations? (default: false for public nets )
-    }
+    },
+    bsc: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://bsc-dataseed1.binance.org')
+      },
+      host: "localhost",
+      port: 8545,
+      network_id: '56'
+    },
+    bsctestnet: {
+      provider: function() {
+        return new HDWalletProvider(mnemonic, 'https://data-seed-prebsc-1-s1.binance.org:8545')
+      },
+      host: "localhost",
+      port: 8545,
+      network_id: '97'
+    },
   },
 
   // Set default mocha options here, use special reporters etc.
@@ -94,16 +110,21 @@ module.exports = {
 
   // Configure your compilers
   compilers: {
-    solc: {
-      version: "0.7.1",    // Fetch exact version from solc-bin (default: truffle's version)
-      // docker: true,        // Use "0.5.1" you've installed locally with docker (default: false)
-      // settings: {          // See the solidity docs for advice about optimization and evmVersion
-      //  optimizer: {
-      //    enabled: false,
-      //    runs: 200
-      //  },
-      //  evmVersion: "byzantium"
-      // }
+    compilers: {
+      solc: {
+        version: '0.7.6',
+        settings: {
+          optimizer: {
+            enabled: true
+          },
+        }
+      }
+    },
+    plugins: [
+      'truffle-plugin-verify'
+    ],
+    api_keys: {
+      etherscan: `${process.env.BINANCE_API_KEY}`
+    },
     }
-  }
 }
